4a5a771b658590f8acf6574aeb3f0056291a6a5e
TwistedFerrisWheel <austin.turner6334@gmail.com>
2021-06-25 15:21:18 -0500

    Rename of IV

--- PasswordManager/Crypt.cs
+++ PasswordManager/Crypt.cs
@@ -14,13 +14,13 @@ namespace PasswordManager
 
         public static string Encrypt(string key, string plainText)
         {
-            byte[] iv = new byte[16];
+            byte[] IV = new byte[16];
             byte[] array;
 
             using (Aes aes = Aes.Create())
             {
                 aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
+                aes.IV = IV;
 
                 ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
@@ -44,13 +44,13 @@ namespace PasswordManager
 
         public static string Decrypt(string key, string cipherText)
         {
-            byte[] iv = new byte[16];
+            byte[] IV = new byte[16];
             byte[] buffer = Convert.FromBase64String(cipherText);
 
             using (Aes aes = Aes.Create())
             {
                 aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
+                aes.IV = IV;
                 ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
 
                 using (MemoryStream memoryStream = new MemoryStream(buffer))
